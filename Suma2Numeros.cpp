e7e693131425ad0de4abc7c0e0dbc250368e2955
Elizabeth Buján <elizabeth.bujan@gmail.com>
2017-09-14 20:16:19 -0500

    AdeP
    Archivos de prueba

---
+++ Suma2Numeros.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n1, n2, resp;
+	
+	cout<<"Ingrese dos numeros: "<<endl;
+	cin>>n1;
+	cin>>n2;
+	
+	resp=n1+n2;
+	
+	cout<<"La respuesta es: "<<resp;
+}
+
